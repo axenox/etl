6468819a46e443d86aa4d216a3fe1ab63b940fc7
aka <aka@NBDR223.salt-solutions.de>
2021-03-23 11:41:26 +0100

    FIX flows with disabled steps were marked as timed out

--- Install/Sql/MySQL/Views/02_flow_run.sql
+++ Install/Sql/MySQL/Views/02_flow_run.sql
@@ -2,14 +2,14 @@ CREATE OR REPLACE VIEW etl_flow_run AS
 SELECT 
 	r.flow_run_oid AS oid,
 	r.flow_oid,
-	COUNT(1) AS steps_run,
+	SUM(r.success_flag + r.error_flag) AS steps_run,
 	SUM(r.error_flag) AS `errors`,
 	SUM(r.step_disabled_flag) AS `steps_disabled`,
 	SUM(r.skipped_flag) AS `steps_skipped`,
 	SUM(r.invalidated_flag) AS `steps_invalidated`,
 	SUM(
 		CASE
-			WHEN r.success_flag = 0 AND DATE_ADD(r.start_time, INTERVAL r.timeout_seconds SECOND) < NOW() THEN 1 
+			WHEN r.success_flag = 0 AND r.end_time IS NULL AND DATE_ADD(r.start_time, INTERVAL r.timeout_seconds SECOND) < NOW() THEN 1 
 			ELSE 0 
 		END
 	) AS steps_timed_out,
