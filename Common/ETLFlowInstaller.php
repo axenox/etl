ba856285977745edf8b9b0fc383f2c13557d8fa1
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-04-23 12:23:29 +0200

    FIX error installing app with ETLflowInstaller from scratch

--- Common/ETLFlowInstaller.php
+++ Common/ETLFlowInstaller.php
@@ -7,6 +7,7 @@ use exface\Core\Interfaces\InstallerContainerInterface;
 use exface\Core\Factories\DataSheetFactory;
 use exface\Core\DataTypes\SortingDirectionsDataType;
 use exface\Core\DataTypes\ComparatorDataType;
+use exface\Core\Exceptions\AppNotFoundError;
 
 /**
  * Makes sure ETL flows and their steps are exported with the apps metamodel
@@ -36,14 +37,23 @@ class ETLFlowInstaller extends MetaModelAdditionInstaller
     {
         parent::__construct($selectorToInstall, $installerContainer);
         
+        // If the app is not installed yet, we don't know it's UID,
+        // but we also don't need it for this case - so just let it
+        // be NULL.
+        try {
+            $appUid = $this->getApp()->getUid();
+        } catch (AppNotFoundError $e) {
+            $appUid = null;
+        }
+        
         $flowSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.ETL.flow');
-        $flowSheet->getFilters()->addConditionFromString('app', $this->getApp()->getUid(), ComparatorDataType::EQUALS);
+        $flowSheet->getFilters()->addConditionFromString('app', $appUid, ComparatorDataType::EQUALS);
         $flowSheet->getSorters()->addFromString('MODIFIED_ON', SortingDirectionsDataType::ASC);
         $flowSheet->getColumns()->addFromAttributeGroup($flowSheet->getMetaObject()->getAttributeGroup('~WRITABLE'));
         $this->addModelDataSheet('ETL', $flowSheet);
         
         $flowSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.ETL.step');
-        $flowSheet->getFilters()->addConditionFromString('flow__app', $this->getApp()->getUid(), ComparatorDataType::EQUALS);
+        $flowSheet->getFilters()->addConditionFromString('flow__app', $appUid, ComparatorDataType::EQUALS);
         $flowSheet->getSorters()->addFromString('MODIFIED_ON', SortingDirectionsDataType::ASC);
         $flowSheet->getColumns()->addFromAttributeGroup($flowSheet->getMetaObject()->getAttributeGroup('~WRITABLE'));
         $this->addModelDataSheet('ETL', $flowSheet);
