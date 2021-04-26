accb42c6081a97234135ecc97e0ab08ee14b56a3
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-04-26 12:34:14 +0200

    NEW presets for ETL prototype configuration

---
+++ Uxon/ETLPrototypeSchema.php
@@ -0,0 +1,13 @@
+<?php
+namespace axenox\ETL\Uxon;
+
+use exface\Core\Uxon\UxonSchema;
+
+/**
+ * UXON schema class for ETL prototypes
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class ETLPrototypeSchema extends UxonSchema
+{}
--- Uxon/SqlColumnMappingSchema.php
+++ Uxon/SqlColumnMappingSchema.php
@@ -1,8 +1,13 @@
 <?php
 namespace axenox\ETL\Uxon;
 
-
 use exface\Core\Uxon\DataSheetMapperSchema;
 
+/**
+ * UXON schema class for SQL column mappings in ETL prototypes
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class SqlColumnMappingSchema extends DataSheetMapperSchema
 {}
