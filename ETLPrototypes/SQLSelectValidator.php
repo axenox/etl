f6570df3374ad0b96d5672c9fcc9de3832c907f4
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-04-28 12:56:22 +0200

    FIX cleaned up SQLSelectValidator

--- ETLPrototypes/SQLSelectValidator.php
+++ ETLPrototypes/SQLSelectValidator.php
@@ -5,11 +5,30 @@ use exface\Core\CommonLogic\DataQueries\SqlDataQuery;
 use axenox\ETL\Common\Traits\SqlIncrementalWhereTrait;
 use axenox\ETL\Interfaces\ETLStepResultInterface;
 
+/**
+ * Performs a SELECT * query on the to-object to check for errors.
+ * 
+ * This prototype is mainly usefull to check views for data inconsistencies, calculation errors, etc. 
+ * 
+ * To avoid reading the entire table or view, make the step incremental by adding `incremental_where` 
+ * and `sql_to_get_current_increment_value` along with placeholders like `[#last_step_increment_value#]` 
+ * to read only entries added after the last run of this step.
+ * 
+ * By defualt this step will execute the followin SQL statement on the data source of the to-object:
+ * 
+ * ```
+ *  SELECT * FROM [#to_object_address#] WHERE [#incremental_where#];
+ * 
+ * ```
+ * 
+ * You can override it by setting the `sql` property.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
 class SQLSelectValidator extends SQLRunner
 {
     use SqlIncrementalWhereTrait;
-    
-    private $maxRows = null;
     
     /**
      * 
@@ -49,16 +68,5 @@ SQL;
         return array_merge(parent::getPlaceholders($stepRunUid, $lastResult), [
             'incremental_where' => $this->getSqlIncrementalWhere() ?? '(1=1)'
         ]);
-    }
-    
-    public function setMaxRows(int $value) : SQLSelectValidator
-    {
-        $this->maxRows = $value;
-        return $this;
-    }
-    
-    protected function getMaxRows() : ?int
-    {
-        return $this->maxRows;
-    }        
+    }      
 }
