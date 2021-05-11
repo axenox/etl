d74a7ee6279d3640044be6f807963b44733d577d
Andrej Kabachnik <andrej.kabachnik@CPX-2CVQ0HY0B2G.dir.svc.accenture.com>
2021-05-11 16:36:16 +0200

    FIX SQL view flow_run_latest

--- Install/Sql/MySQL/Views/03_flow_run_latest.sql
+++ Install/Sql/MySQL/Views/03_flow_run_latest.sql
@@ -1,7 +1,13 @@
 CREATE OR REPLACE VIEW etl_flow_run_latest AS
 SELECT 
 	r.flow_oid,
-	r.flow_run_oid AS flow_run_oid
-FROM etl_step_run r
-ORDER BY start_time DESC
-LIMIT 1;
+	MAX(r.flow_run_oid) AS flow_run_oid
+FROM (
+	SELECT 
+			r1.flow_oid,
+			MAX(r1.start_time) AS start_time
+		FROM etl_step_run r1 
+		GROUP BY r1.flow_oid
+	) r2 
+	INNER JOIN etl_step_run r ON r.flow_oid = r2.flow_oid AND r.start_time = r2.start_time
+GROUP BY flow_oid;
