5170acf1706ebc27409fd2035802fb9135ca5e53
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2022-10-28 22:51:53 +0200

    FIX improved object categorization and data flow charts

--- Install/Sql/MsSQL/Views/01_flow_objects.sql
+++ Install/Sql/MsSQL/Views/01_flow_objects.sql
@@ -5,47 +5,148 @@ GO
 CREATE VIEW [dbo].[etl_flow_objects] ( 
 	flow_oid,
 	object_oid,
-	level,
-	type
+	type,
+	level
 )
 AS
 	SELECT
-		u.flow_oid,
-		u.object_oid,
-		ROUND(AVG(u.[level]), 0) AS 'level',
-		CASE
-			WHEN ROUND(AVG(u.[level]), 0) <= 1 THEN 'E'
-			WHEN ROUND(AVG(u.[level]), 0) < 3 THEN 'T'
-			WHEN ROUND(AVG(u.[level]), 0) >= 3 THEN 'L'
-		END AS 'type'
-	FROM 
+		l2.flow_oid,
+		l2.object_oid,
+		(CASE
+			WHEN l2.out_e > 0 THEN 'E'
+			WHEN l2.in_l > 0 THEN 'L'
+			WHEN l2.out_t > 0 AND l2.[in] = 0 THEN 'L'
+			ELSE 'T'
+		END) AS [type],
+		(CASE
+			WHEN l2.out_e > 0 THEN 1
+			WHEN l2.in_l > 0 THEN 3
+			WHEN l2.out_t > 0 AND l2.[in] = 0 THEN 3
+			ELSE 2
+		END) AS [level]
+	FROM
 	(
-		(
-			SELECT 
-				se.from_object_oid AS object_oid,
-				se.flow_oid AS flow_oid,
-				[type],
-				CASE se.[type]
-					WHEN 'E' THEN 1
-					WHEN 'T' THEN 3
-					WHEN 'L' THEN 2
-				END AS 'level'
-			FROM [dbo].[etl_step] se
-			WHERE se.from_object_oid IS NOT NULL
-		)
-		UNION ALL
-		(
-			SELECT 
-				stl.[to_object_oid] AS object_oid,
-				stl.[flow_oid] AS flow,
-				stl.[type],
-				CASE stl.[type]
-					WHEN 'E' THEN 2
-					WHEN 'T' THEN 2
-					WHEN 'L' THEN 3
-				END AS 'level'
-			FROM [dbo].[etl_step] stl
-		) 
-	) u
-	GROUP BY u.flow_oid, u.object_oid
+		SELECT 
+			l1.flow_oid,
+			l1.object_oid,
+			SUM([in]) AS 'in',
+			SUM([out]) AS 'out',
+			SUM(in_e) AS in_e,
+			SUM(out_e) AS out_e,
+			SUM(in_t) AS in_t,
+			SUM(out_t) AS out_t,
+			SUM(in_l) AS in_l,
+			SUM(out_l) AS out_l
+		FROM
+			(
+				(
+					SELECT
+						from_object_oid AS object_oid,
+						flow_oid,
+						0 AS 'in',
+						1 AS 'out',
+						0 AS 'in_e',
+						1 AS 'out_e',
+						0 AS 'in_t',
+						0 AS 'out_t',
+						0 AS 'in_l',
+						0 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'E'
+						AND from_object_oid IS NOT NULL
+				) 
+				UNION ALL
+				(
+					SELECT
+						to_object_oid AS object_oid,
+						flow_oid,	
+						1 AS 'in',
+						0 AS 'out',					
+						1 AS 'in_e',
+						0 AS 'out_e',
+						0 AS 'in_t',
+						0 AS 'out_t',
+						0 AS 'in_l',
+						0 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'E'
+						AND to_object_oid IS NOT NULL
+				) 
+				UNION ALL
+				(
+					SELECT
+						from_object_oid AS object_oid,
+						flow_oid,
+						0 AS 'in',
+						1 AS 'out',
+						0 AS 'in_e',
+						0 AS 'out_e',
+						0 AS 'in_t',
+						1 AS 'out_t',
+						0 AS 'in_l',
+						0 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'T'
+						AND from_object_oid IS NOT NULL
+				) 
+				UNION ALL
+				(
+					SELECT
+						to_object_oid AS object_oid,
+						flow_oid,	
+						1 AS 'in',
+						0 AS 'out',	
+						0 AS 'in_e',
+						0 AS 'out_e',
+						1 AS 'in_t',
+						0 AS 'out_t',
+						0 AS 'in_l',
+						0 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'T'
+						AND to_object_oid IS NOT NULL
+				) 
+				UNION ALL
+				(
+					SELECT
+						from_object_oid AS object_oid,
+						flow_oid,
+						0 AS 'in',
+						1 AS 'out',
+						0 AS 'in_e',
+						0 AS 'out_e',
+						0 AS 'in_t',
+						0 AS 'out_t',
+						0 AS 'in_l',
+						1 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'L'
+						AND from_object_oid IS NOT NULL
+				) 
+				UNION ALL
+				(
+					SELECT
+						to_object_oid AS object_oid,
+						flow_oid,	
+						1 AS 'in',
+						0 AS 'out',	
+						0 AS 'in_e',
+						0 AS 'out_e',
+						0 AS 'in_t',
+						0 AS 'out_t',
+						1 AS 'in_l',
+						0 AS 'out_l'
+					FROM
+						etl_step es
+					WHERE type = 'L'
+						AND to_object_oid IS NOT NULL
+				) 
+			) l1
+		GROUP BY l1.flow_oid, l1.object_oid
+	) l2
 ;
