f57460c13ec137913e92eeebe72071886ea56776
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-12-15 14:38:00 +0100

    FIX SQL migration

--- Install/Sql/MySQL/Migrations/1.01/ 20231112_1121_1_NEW_webserviceTyp.sql
+++ Install/Sql/MySQL/Migrations/1.01/ 20231112_1121_1_NEW_webserviceTyp.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `etl_webservice_type` (
   `app_oid` binary(16) DEFAULT NULL,
   `schema_json` longtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   PRIMARY KEY (`oid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
 
 ALTER TABLE `etl_webservice_route`
 ADD `type_oid` binary(16) NULL;
@@ -23,7 +23,7 @@ ADD `response_header` text COLLATE 'utf8mb3_general_ci' NULL AFTER `response_bod
 
 -- DOWN
 
-DROP TABLE `etl_webservice_type`
+DROP TABLE `etl_webservice_type`;
 
 ALTER TABLE `etl_webservice_route`
 DROP COLUMN `type_oid`;
