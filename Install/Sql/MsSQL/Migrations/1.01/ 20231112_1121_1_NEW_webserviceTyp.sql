ffefe6f2a0924e25acde093d5e5ba344e61b0489
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2023-12-19 12:17:31 +0100

    FIX SQL migrations

--- Install/Sql/MsSQL/Migrations/1.01/ 20231112_1121_1_NEW_webserviceTyp.sql
+++ Install/Sql/MsSQL/Migrations/1.01/ 20231112_1121_1_NEW_webserviceTyp.sql
@@ -1,27 +1,47 @@
 -- UP
-IF OBJECT_ID ('etl_webservice_type', N'U') IS NULL 
-CREATE TABLE 'etl_webservice_type' (
-CREATE TABLE IF NOT EXISTS  (
-  'oid' binary(16) NOT NULL,
-  'created_on' datetime NOT NULL,
-  'modified_on' datetime NOT NULL,
-  'created_by_user_oid' binary(16) NOT NULL,
-  'modified_by_user_oid' binary(16) NOT NULL,
-  'name' varchar(100) NOT NULL,
-  'version' varchar(50),
-  'app_oid' binary(16) DEFAULT NULL,
-  'schema_json' nvarchar(max),
-  PRIMARY KEY ('oid')
 
-ALTER TABLE 'etl_webservice_route'
-ADD 'type_oid' binary(16) NULL;
+-- Service types
+IF OBJECT_ID ('etl_webservice_type', N'U') IS NULL 
+CREATE TABLE etl_webservice_type (
+  oid binary(16) NOT NULL,
+  created_on datetime NOT NULL,
+  modified_on datetime NOT NULL,
+  created_by_user_oid binary(16) NOT NULL,
+  modified_by_user_oid binary(16) NOT NULL,
+  name nvarchar(100) NOT NULL,
+  version nvarchar(50),
+  app_oid binary(16) DEFAULT NULL,
+  schema_json nvarchar(max),
+  PRIMARY KEY (oid)
+);
+
+IF COL_LENGTH('dbo.etl_webservice_route', 'type_oid') IS NULL
+ALTER TABLE dbo.etl_webservice_route
+	ADD type_oid binary(16) NULL;
 
 -- for validation of schema:
-ALTER TABLE 'etl_webservice_request'
-ADD 'response_body' nvarchar(max) NULL,
-ADD 'response_header' varchar(500) NULL;
+IF COL_LENGTH('dbo.etl_webservice_request', 'response_body') IS NULL
+ALTER TABLE dbo.etl_webservice_request
+	ADD response_body nvarchar(max) NULL;
+IF COL_LENGTH('dbo.etl_webservice_request', 'response_header') IS NULL
+ALTER TABLE etl_webservice_request
+	ADD response_header nvarchar(max) NULL;
 
 -- DOWN
+
+IF COL_LENGTH('dbo.etl_webservice_request', 'response_body') IS NOT NULL
+ALTER TABLE etl_webservice_request
+	DROP COLUMN response_body;
+IF COL_LENGTH('dbo.etl_webservice_request', 'response_header') IS NOT NULL
+ALTER TABLE etl_webservice_request
+	DROP COLUMN response_header;
+
+IF COL_LENGTH('dbo.etl_webservice_route','type_oid') IS NOT NULL
+ALTER TABLE dbo.etl_webservice_route
+DROP COLUMN type_oid;
+
+/* DROP TABLE etl_webservice_type */
+
 DECLARE @table NVARCHAR(max) = 'etl_webservice_type';
 DECLARE @schema NVARCHAR(max) = 'dbo';
 DECLARE @stmt NVARCHAR(max)
@@ -37,8 +57,5 @@ BEGIN
 	EXEC(@stmt);
 
 	-- drop the table itself
-	DROP TABLE CONCAT(@schema, '.', @table);
+	EXEC('DROP TABLE ' + @schema + '.' + @table);
 END
-
-ALTER TABLE CONCAT(@schema, '.', 'etl_webservice_route');
-DROP COLUMN 'type_oid';
