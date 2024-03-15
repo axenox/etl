604a747a3f08e1c5785cb0bf7bfe398c24cdaf98
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2024-03-15 17:26:21 +0100

    FIX improved SQL migrations for step sequence

--- Install/Sql/MsSQL/Migrations/1.01/20240314_0920_01_NEW_etl_step_with_step_flow_sequence.sql
+++ Install/Sql/MsSQL/Migrations/1.01/20240314_0920_01_NEW_etl_step_with_step_flow_sequence.sql
@@ -1,31 +1,55 @@
 -- UP
 IF COL_LENGTH('dbo.etl_step','step_flow_sequence') IS NULL
 BEGIN
-    DECLARE @addQuery NVARCHAR(MAX);
-    SET @addQuery = 'ALTER TABLE [dbo].[etl_step] ADD [step_flow_sequence] INT NULL;'
-    EXEC sp_executesql @addQuery;
-
-    DECLARE @updateQuery NVARCHAR(MAX);
-    SET @updateQuery =
-        'UPDATE  s
-        SET s.[step_flow_sequence] = srs.[flow_run_pos]
-        FROM [dbo].[etl_step] s
-        INNER JOIN (
-          SELECT DISTINCT flow_run_pos, sr.flow_oid, sr.step_oid FROM etl_step_run sr
-          LEFT JOIN etl_flow_run fr ON fr.oid = sr.flow_run_oid
-          WHERE  fr.valid_flag = 1 AND fr.created_on = (Select MAX(created_on) FROM etl_flow_run WHERE flow_oid = fr.flow_oid)
-        ) srs ON etl_step.oid = srs.step_oid AND etl_step.flow_oid = srs.flow_oid;'
-    EXEC sp_executesql @updateQuery;
+    ALTER TABLE [dbo].[etl_step] ADD [step_flow_sequence] INT NULL;
+    
+    /* Get the sequence from last successful run */
+    UPDATE etl_step SET step_flow_sequence = (
+    	SELECT
+    		srl.flow_run_pos
+    	FROM 
+			(
+				SELECT TOP 1
+					r.flow_run_oid AS oid,
+					r.flow_oid,
+					(CASE 
+						WHEN SUM(r.success_flag) + SUM(r.step_disabled_flag) + SUM(r.skipped_flag) - SUM(r.invalidated_flag) = COUNT(1) THEN 1 
+						ELSE 0 
+					END) AS 'valid_flag',
+					MIN(r.created_on) AS created_on
+				FROM [dbo].[etl_step_run] r
+					WHERE r.flow_oid = etl_step.flow_oid
+				GROUP BY r.flow_run_oid, r.flow_oid
+				HAVING (CASE 
+						WHEN SUM(r.success_flag) + SUM(r.step_disabled_flag) + SUM(r.skipped_flag) - SUM(r.invalidated_flag) = COUNT(1) THEN 1 
+						ELSE 0 
+					END) = 1
+				ORDER BY MIN(r.created_on) DESC
+			) frl
+			LEFT JOIN etl_step_run srl ON srl.flow_run_oid = frl.oid
+			WHERE srl.step_oid = etl_step.oid
+		)
+		WHERE step_flow_sequence IS NULL OR step_flow_sequence = -1;
+	
+	/* Simple flows with only one step get the sequence 1 if not processed already */	
+	UPDATE etl_step SET step_flow_sequence = 1 
+		WHERE (SELECT COUNT(*) FROM etl_step s1 WHERE s1.flow_oid = etl_step.flow_oid) = 1
+			AND step_flow_sequence IS NULL OR step_flow_sequence = -1;
+	
+	/* All other steps get -1 to indicate, that they must be reviewed! */
+	UPDATE etl_step SET step_flow_sequence = -1 WHERE step_flow_sequence IS NULL;
 
     DECLARE @alterQuery NVARCHAR(MAX);
     SET @alterQuery = 'ALTER TABLE [dbo].[etl_step] ALTER COLUMN [step_flow_sequence] INT NOT NULL;'
     EXEC sp_executesql @alterQuery;
-
-    -- delete old value
-    DECLARE @alterQuery2 NVARCHAR(MAX);
-    SET @alterQuery2 = 'ALTER TABLE [dbo].[etl_step] DROP COLUMN [run_after_step_oid];'
-    EXEC sp_executesql @alterQuery2;
 END
 
+IF COL_LENGTH('dbo.etl_step','run_after_step_oid') IS NOT NULL
+ALTER TABLE [dbo].[etl_step] DROP COLUMN [run_after_step_oid];
+
 -- DOWN
+
+IF COL_LENGTH('dbo.etl_step','run_after_step_oid') IS NULL
+ALTER TABLE [dbo].[etl_step] ADD [run_after_step_oid] BINARY(16) NULL;
+
 -- cannot change back structural changes
