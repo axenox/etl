78f286b387085a99c1812bc632a8220e8eecf0a6
Andrej Kabachnik <andrej.kabachnik@salt-solutions.de>
2024-03-16 16:11:35 +0100

    FIX error in MS SQL migration

--- Install/Sql/MsSQL/Migrations/1.15/20240314_0920_1_NEW_etl_step_with_step_flow_sequence.sql
+++ Install/Sql/MsSQL/Migrations/1.15/20240314_0920_1_NEW_etl_step_with_step_flow_sequence.sql
@@ -4,6 +4,7 @@ BEGIN
     ALTER TABLE [dbo].[etl_step] ADD [step_flow_sequence] INT NULL;
     
     /* Get the sequence from last successful run */
+    EXEC sys.sp_executesql @query = N'
     UPDATE etl_step SET step_flow_sequence = (
     	SELECT
     		srl.flow_run_pos
@@ -15,7 +16,7 @@ BEGIN
 					(CASE 
 						WHEN SUM(r.success_flag) + SUM(r.step_disabled_flag) + SUM(r.skipped_flag) - SUM(r.invalidated_flag) = COUNT(1) THEN 1 
 						ELSE 0 
-					END) AS 'valid_flag',
+					END) AS valid_flag,
 					MIN(r.created_on) AS created_on
 				FROM [dbo].[etl_step_run] r
 					WHERE r.flow_oid = etl_step.flow_oid
@@ -30,18 +31,21 @@ BEGIN
 			WHERE srl.step_oid = etl_step.oid
 		)
 		WHERE step_flow_sequence IS NULL OR step_flow_sequence = -1;
+	';
 	
 	/* Simple flows with only one step get the sequence 1 if not processed already */	
+	EXEC sys.sp_executesql @query = N'
 	UPDATE etl_step SET step_flow_sequence = 1 
 		WHERE (SELECT COUNT(*) FROM etl_step s1 WHERE s1.flow_oid = etl_step.flow_oid) = 1
 			AND step_flow_sequence IS NULL OR step_flow_sequence = -1;
+	';
 	
 	/* All other steps get -1 to indicate, that they must be reviewed! */
+	EXEC sys.sp_executesql @query = N'
 	UPDATE etl_step SET step_flow_sequence = -1 WHERE step_flow_sequence IS NULL;
+	';
 
-    DECLARE @alterQuery NVARCHAR(MAX);
-    SET @alterQuery = 'ALTER TABLE [dbo].[etl_step] ALTER COLUMN [step_flow_sequence] INT NOT NULL;'
-    EXEC sp_executesql @alterQuery;
+    ALTER TABLE [dbo].[etl_step] ALTER COLUMN [step_flow_sequence] INT NOT NULL;
 END
 
 IF COL_LENGTH('dbo.etl_step','run_after_step_oid') IS NOT NULL
