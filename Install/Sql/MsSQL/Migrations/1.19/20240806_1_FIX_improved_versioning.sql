d096d4de87c11b1ce57844ca73dba4e9cdecdcf2
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-08-06 21:20:36 +0200

    FIX MS SQL migration error

--- Install/Sql/MsSQL/Migrations/1.19/20240806_1_FIX_improved_versioning.sql
+++ Install/Sql/MsSQL/Migrations/1.19/20240806_1_FIX_improved_versioning.sql
@@ -9,7 +9,38 @@ ALTER TABLE dbo.etl_flow ALTER COLUMN version NVARCHAR(50) NULL;
 
 -- Remove request_direction from web services
 IF COL_LENGTH('dbo.etl_webservice','request_direction') IS NOT NULL
-ALTER TABLE dbo.etl_webservice DROP COLUMN request_direction;
+BEGIN
+	DECLARE @sql NVARCHAR(MAX),
+			@schema NVARCHAR(50) = 'dbo',
+			@table NVARCHAR(50) = 'etl_webservice',
+			@column NVARCHAR(50) = 'request_direction'
+	/* DROP default constraints	*/
+	WHILE 1=1
+	BEGIN
+		SELECT TOP 1 @sql = N'ALTER TABLE '+@schema+'.'+@table+' DROP CONSTRAINT ['+dc.NAME+N']'
+			FROM sys.default_constraints dc
+				JOIN sys.columns c ON c.default_object_id = dc.object_id
+			WHERE 
+				dc.parent_object_id = OBJECT_ID(@table)
+				AND c.name = @column
+		IF @@ROWCOUNT = 0 BREAK
+		EXEC (@sql)
+	END
+	/* DROP foreign keys */
+	WHILE 1=1
+	BEGIN
+		SELECT TOP 1 @sql = N'ALTER TABLE '+@schema+'.'+@table+' DROP CONSTRAINT ['+fk.NAME+N']'
+			FROM sys.foreign_keys fk
+				JOIN sys.foreign_key_columns fk_cols ON fk_cols.constraint_object_id = fk.object_id
+			WHERE 
+				fk.parent_object_id = OBJECT_ID(@table)
+				AND COL_NAME(fk.parent_object_id, fk_cols.parent_column_id) = @column
+		IF @@ROWCOUNT = 0 BREAK
+		EXEC (@sql)
+	END
+	/* DROP column */
+	EXEC(N'ALTER TABLE ['+@schema+'].['+@table+'] DROP COLUMN ['+@column+']')
+END
 
 -- Make local url required
 IF COL_LENGTH('dbo.etl_webservice','local_url') IS NOT NULL
